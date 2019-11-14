014190dd99afe58eb3378ae62176548476bf5ce1
Abdul Samad <samk8778@yahoo.com>
2019-11-14 13:07:43 +0530

    :construction: Options Appearance Working

--- src/js/libs.js
+++ src/js/libs.js
@@ -4,3 +4,66 @@ import 'materialize-css/dist/js/materialize';
 document.addEventListener('DOMContentLoaded', function() {
 	M.AutoInit();
 });
+
+/* Get All Keys */
+/* chrome.storage.sync.get(null, function(items) {
+	var allKeys = Object.keys(items);
+	console.log(allKeys);
+}); */
+
+// Change Theme
+function changeTheme(theme) {
+	// prettier-ignore
+	if (theme == 'dark') {
+		document.documentElement.style.setProperty('--color', '#f2f2f2');
+		document.documentElement.style.setProperty('--opposite-color', '#fff');
+		document.documentElement.style.setProperty('--background-color', '#000');
+		document.documentElement.style.setProperty('--options-background-color', '#1f1f1f');
+		document.documentElement.style.setProperty('--select-hover-color', '#2f2f2f');
+		document.documentElement.style.setProperty('--card-shadow', '0 2px 2px 0 rgba(255, 255, 255, 0.14), 0 3px 1px -2px rgba(255, 255, 255, 0.12),0 1px 5px 0 rgba(255, 255, 255, 0.2)');
+	} else if (theme == 'light') {
+		document.documentElement.style.setProperty('--color', '#2f2f2f');
+		document.documentElement.style.setProperty('--opposite-color', '#000');
+		document.documentElement.style.setProperty('--background-color', '#f5f5f5');
+		document.documentElement.style.setProperty('--options-background-color', '#f5f5f5');
+		document.documentElement.style.setProperty('--select-hover-color', '#f2f2f2');
+		document.documentElement.style.setProperty('--card-shadow', '0 2px 2px 0 rgba(0, 0, 0, 0.14), 0 3px 1px -2px rgba(0, 0, 0, 0.12), 0 1px 5px 0 rgba(0, 0, 0, 0.2)');
+	}
+}
+
+// Fetch Settings
+function fetchSettings() {
+	chrome.storage.sync.get(['theme', 'font', 'accent'], function(result) {
+		// Theme
+		if (result.theme != undefined) {
+			const theme = document.querySelector('#theme');
+			if (theme) {
+				theme.value = result.theme;
+			}
+			changeTheme(result.theme);
+		}
+
+		// Font
+		if (result.font != undefined) {
+			const sel = document.querySelector('#font');
+			if (sel) {
+				M.FormSelect.init(sel, {});
+				sel.innerHTML = `<option value="${result.font}">${result.font}</option>`;
+				sel.value = result.font;
+			}
+			document.body.style.fontFamily = result.font;
+		}
+
+		// Accent
+		if (result.accent != undefined) {
+			const accentCollection = document.querySelector('.accent-collection');
+			if (accentCollection) {
+				document.querySelector('.active').classList.remove('active');
+				document.querySelectorAll(`[data-color="${result.accent}"]`)[0].classList.add('active');
+			}
+			document.documentElement.style.setProperty('--accent', result.accent);
+		}
+	});
+}
+fetchSettings();
+chrome.storage.onChanged.addListener(fetchSettings);
--- src/js/options.js
+++ src/js/options.js
@@ -1,15 +1,51 @@
 console.time('hello');
+/* Imports */
+// import 'materialize-css/js/cash';
+// import 'materialize-css/js/component';
+// import 'materialize-css/js/global';
+
+// import 'materialize-css/js/dropdown';
+// import 'materialize-css/js/forms';
+// import 'materialize-css/js/select';
+// import 'materialize-css/js/modal';
+// import 'materialize-css/js/waves';
+
 /* Theme */
+document.querySelector('#theme').addEventListener(
+	'change',
+	function(ev) {
+		if (this.value !== 'default') {
+			const value = this.value;
+			chrome.storage.sync.set({ theme: value });
+		}
+	},
+	false,
+);
 
 /* Font */
 chrome.fontSettings.getFontList(function(list) {
-	const sel = document.querySelector('#selectFont');
+	const sel = document.querySelector('#font');
 	list.forEach(function(value) {
-		const opt = document.createElement('option');
-		opt.appendChild(document.createTextNode(value.displayName));
-		opt.value = value.displayName;
-		sel.appendChild(opt);
+		if (sel.value !== value.displayName) {
+			const opt = document.createElement('option');
+			opt.appendChild(document.createTextNode(value.displayName));
+			opt.value = value.displayName;
+			sel.appendChild(opt);
+		}
 	});
+
+	document.querySelector('#font').addEventListener(
+		'change',
+		function(ev) {
+			document.body.style.fontFamily = this.value;
+			if (this.value !== 'default') {
+				const value = this.value;
+				chrome.storage.sync.set({ font: value });
+			}
+		},
+		false,
+	);
+	M.FormSelect.init(sel, {});
 });
 
 /* Accent Color */
@@ -19,12 +55,21 @@ document.querySelectorAll('.accent-btn').forEach(function(elem) {
 		function() {
 			document.querySelector('.active').classList.remove('active');
 			this.classList.add('active');
+			document.documentElement.style.setProperty('--accent', this.dataset.color);
+			chrome.storage.sync.set({ accent: this.dataset.color });
 		},
 		false,
 	);
 });
 
 /* Sorting */
+document.querySelector('#sort').addEventListener(
+	'change',
+	function() {
+		console.log(this.value);
+	},
+	false,
+);
 
 /* Incognito Selection */
 document.querySelector('#incognitoCheckbox').addEventListener(
@@ -33,11 +78,9 @@ document.querySelector('#incognitoCheckbox').addEventListener(
 		const container = document.querySelector('#excludedEntriesContainer');
 		if (this.checked === true) {
 			// Checked
-			console.log('Checked');
 			container.classList.remove('hide');
 		} else {
 			// Unchecked
-			console.log('Unchecked');
 			container.classList.add('hide');
 		}
 	},
@@ -49,7 +92,12 @@ document.querySelector('#addExclusion').addEventListener(
 	'click',
 	function(value) {
 		const body = document.querySelector('#excluded-entries-body');
-		body.innerHTML += `<tr><td>Google</td><td>https://google.com</td><td><button class="btn-small waves-effect waves-light red accent-4">Remove</button></td></tr>`;
+		body.innerHTML += `
+		<tr>
+			<td>Google</td>
+			<td>https://google.com</td>
+			<td><button class="btn-small waves-effect waves-light red accent-4">Remove</button></td>
+		</tr>`;
 	},
 	false,
 );
