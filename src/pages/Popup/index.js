e0bbc8cbc348f67b4801dec60cfd50978cf3cfdf
Abdul Samad <Samk8778@yahoo.com>
2020-07-05 18:15:28 +0530

    :art: Separated CSS files

--- src/pages/Popup/index.js
+++ src/pages/Popup/index.js
@@ -2,6 +2,6 @@ import React from 'react';
 import { render } from 'react-dom';
 
 import Popup from './Popup';
-import './index.css';
+import './css/Popup.css';
 
 render(<Popup />, window.document.querySelector('#app-container'));
