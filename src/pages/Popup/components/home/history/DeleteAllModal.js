14a8bf39836385aa62822fab8665765c5ee99923
Abdul Samad <Samk8778@yahoo.com>
2020-07-07 03:51:13 +0530

    :construction: Added delete all modal

---
+++ src/pages/Popup/components/home/history/DeleteAllModal.js
@@ -0,0 +1,67 @@
+import React from 'react';
+import Button from '@material-ui/core/Button';
+import Dialog from '@material-ui/core/Dialog';
+import DialogActions from '@material-ui/core/DialogActions';
+import DialogContent from '@material-ui/core/DialogContent';
+import DialogContentText from '@material-ui/core/DialogContentText';
+import DialogTitle from '@material-ui/core/DialogTitle';
+import Slide from '@material-ui/core/Slide';
+import DeleteForeverIcon from '@material-ui/icons/DeleteForever';
+import BottomNavigationAction from '@material-ui/core/BottomNavigationAction';
+
+const Transition = React.forwardRef(function Transition(props, ref) {
+  return <Slide direction="up" ref={ref} {...props} />;
+});
+
+function DeleteModal() {
+  const [open, setOpen] = React.useState(false);
+
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  return (
+    <>
+      <BottomNavigationAction
+        onClick={handleClickOpen}
+        icon={<DeleteForeverIcon />}
+      />
+      <Dialog
+        open={open}
+        TransitionComponent={Transition}
+        keepMounted
+        onClose={handleClose}
+        aria-labelledby="alert-dialog-slide-title"
+        aria-describedby="alert-dialog-slide-description"
+      >
+        <DialogTitle id="alert-dialog-slide-title">
+          {'Delete History Item?'}
+        </DialogTitle>
+        <DialogContent>
+          <DialogContentText id="alert-dialog-slide-description">
+            <strong>Note:</strong> Deleting this also deletes this from your
+            browser history.
+          </DialogContentText>
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleClose} color="primary">
+            Cancel
+          </Button>
+          <Button
+            onClick={() =>
+              chrome.history.deleteAll(() => window.location.reload())
+            }
+            color="secondary"
+          >
+            Delete
+          </Button>
+        </DialogActions>
+      </Dialog>
+    </>
+  );
+}
+export default DeleteModal;
