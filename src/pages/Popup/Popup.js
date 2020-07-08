ffb3264e5190c5ec32e4f4b2b6d31488ce32aff2
Abdul Samad <Samk8778@yahoo.com>
2020-07-09 02:58:02 +0530

    :lipstick: Added theme type

--- src/pages/Popup/Popup.js
+++ src/pages/Popup/Popup.js
@@ -1,18 +1,31 @@
-import React from 'react';
+import React, { useMemo } from 'react';
+import { createMuiTheme, ThemeProvider } from '@material-ui/core/styles';
 import Navbar from './components/layout/Navbar';
 import BottomNavbar from './components/layout/BottomNavbar';
 import PopupState from './context/popupState';
 import Home from './components/home/Home';
 
 const Popup = () => {
+  const theme = useMemo(
+    () =>
+      createMuiTheme({
+        palette: {
+          type: 'light',
+        },
+      }),
+    []
+  );
+
   return (
-    <PopupState>
-      <div className="App">
-        <Navbar title="Peek History" />
-        <Home />
-        <BottomNavbar />
-      </div>
-    </PopupState>
+    <ThemeProvider theme={theme}>
+      <PopupState>
+        <div className="App">
+          <Navbar title="Peek History" />
+          <Home />
+          <BottomNavbar />
+        </div>
+      </PopupState>
+    </ThemeProvider>
   );
 };
 
