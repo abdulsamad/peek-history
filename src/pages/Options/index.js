aa99f574d1ad4472ed271b9b43e7fd550ddd84e2
Abdul Samad <Samk8778@yahoo.com>
2020-07-20 02:16:18 +0530

    :fire: Removed separate CSS files

--- src/pages/Options/index.js
+++ src/pages/Options/index.js
@@ -2,6 +2,5 @@ import React from 'react';
 import { render } from 'react-dom';
 
 import Options from './Options';
-import './css/Options.css';
 
 render(<Options />, window.document.querySelector('#app-container'));
--- src/pages/Popup/index.js
+++ src/pages/Popup/index.js
@@ -2,6 +2,5 @@ import React from 'react';
 import { render } from 'react-dom';
 
 import Popup from './Popup';
-import './css/Popup.css';
 
 render(<Popup />, window.document.querySelector('#app-container'));
