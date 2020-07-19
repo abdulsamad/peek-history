1ce9dda659b41f69946011167f86dcd078171291
Abdul Samad <Samk8778@yahoo.com>
2020-07-20 02:22:05 +0530

    :art: Improved structure by separating theme provider

--- src/pages/Options/Options.js
+++ src/pages/Options/Options.js
@@ -1,53 +1,21 @@
-import React, { useMemo } from 'react';
-import { createMuiTheme, ThemeProvider } from '@material-ui/core/styles';
-
+import React from 'react';
+import { OptionsProvider } from './context/optionsContext';
+import ThemeProvider from './components/layout/ThemeProvider';
 import Navbar from './components/layout/Navbar';
 import Footer from './components/layout/Footer';
 import Appearance from './components/appearance/Appearance';
 import Settings from './components/settings/Settings';
 
 function Options() {
-	const theme = useMemo(
-		() =>
-			createMuiTheme({
-				palette: {
-					type: 'dark',
-					primary: {
-						// light: will be calculated from palette.primary.main,
-						main: '#64B5F6',
-						// dark: will be calculated from palette.primary.main,
-						// contrastText: will be calculated to contrast with palette.primary.main
-					},
-
-					secondary: {
-						// light: '#0066ff',
-						main: '#0044ff',
-						// dark: will be calculated from palette.secondary.main,
-						// contrastText: '#ffcc00',
-					},
-					background: {
-						default: '#000000',
-						paper: '#000000',
-						container: '#000000',
-					},
-					text: {
-						primary: '#fff',
-						secondary: '#e3d3d3',
-					},
-				},
-			}),
-		[],
-	);
-
 	return (
-		<ThemeProvider theme={theme}>
-			<div className='App'>
+		<OptionsProvider>
+			<ThemeProvider>
 				<Navbar title='Peek History' />
 				<Appearance />
 				<Settings />
 				<Footer title='Peek History' />
-			</div>
-		</ThemeProvider>
+			</ThemeProvider>
+		</OptionsProvider>
 	);
 }
 
--- src/pages/Popup/Popup.js
+++ src/pages/Popup/Popup.js
@@ -1,32 +1,20 @@
-import React, { useMemo } from 'react';
-import { createMuiTheme, ThemeProvider } from '@material-ui/core/styles';
+import React from 'react';
+import { PopupProvider } from './context/popupContext';
+import ThemeProvider from './components/layout/ThemeProvider';
 import Navbar from './components/layout/Navbar';
 import BottomNavbar from './components/layout/BottomNavbar';
-import PopupState from './context/popupState';
 import Home from './components/home/Home';
 
-const Popup = () => {
-	const theme = useMemo(
-		() =>
-			createMuiTheme({
-				palette: {
-					type: 'dark',
-				},
-			}),
-		[],
-	);
-
+function Popup() {
 	return (
-		<ThemeProvider theme={theme}>
-			<PopupState>
-				<div className='App'>
-					<Navbar title='Peek History' />
-					<Home />
-					<BottomNavbar />
-				</div>
-			</PopupState>
-		</ThemeProvider>
+		<PopupProvider>
+			<ThemeProvider>
+				<Navbar title='Peek History' />
+				<Home />
+				<BottomNavbar />
+			</ThemeProvider>
+		</PopupProvider>
 	);
-};
+}
 
 export default Popup;
