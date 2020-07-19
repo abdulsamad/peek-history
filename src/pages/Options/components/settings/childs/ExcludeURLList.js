02211c7aa7e33273b34926f62750f4313c484cd5
Abdul Samad <Samk8778@yahoo.com>
2020-07-20 02:41:51 +0530

    :art: Added delete component

--- src/pages/Options/components/settings/childs/ExcludeURLList.js
+++ src/pages/Options/components/settings/childs/ExcludeURLList.js
@@ -1,8 +1,8 @@
 import React from 'react';
+import { useOptionsState } from '../../../context/optionsContext';
 import {
 	makeStyles,
 	Grid,
-	Typography,
 	Table,
 	TableBody,
 	TableCell,
@@ -10,8 +10,9 @@ import {
 	TableHead,
 	TableRow,
 	Paper,
-	Button,
 } from '@material-ui/core';
+import DeleteModal from './DeleteModal';
+import DeleteAllModal from './DeleteAllModal';
 
 const useStyles = makeStyles({
 	table: {
@@ -24,46 +25,51 @@ const useStyles = makeStyles({
 	},
 });
 
-export default function SimpleTable() {
+function ExcludeURLList() {
+	const { incognito, excludedURLs } = useOptionsState();
 	const classes = useStyles();
 
 	return (
-		<Grid container>
-			<Grid item md={1}></Grid>
-			<Grid item md={10}>
-				<TableContainer component={Paper}>
-					<Table className={classes.table} aria-label='simple table'>
-						<TableHead>
-							<TableRow>
-								<TableCell align='center'>Domain</TableCell>
-								<TableCell align='center'>URL</TableCell>
-								<TableCell align='center'>-</TableCell>
-							</TableRow>
-						</TableHead>
-						<TableBody>
-							<TableRow>
-								<TableCell component='th' align='center' scope='row'>
-									Google.com
-								</TableCell>
-								<TableCell align='center'>https://google.com</TableCell>
-								<TableCell align='center'>
-									<Button variant='contained' color='secondary'>
-										Delete
-									</Button>
-								</TableCell>
-							</TableRow>
-							<TableRow>
-								<TableCell scope='row' colSpan={3} padding='none'>
-									<Button variant='contained' fullWidth={true}>
-										Delete All
-									</Button>
-								</TableCell>
-							</TableRow>
-						</TableBody>
-					</Table>
-				</TableContainer>
-			</Grid>
-			<Grid item md={1}></Grid>
-		</Grid>
+		<>
+			{incognito && excludedURLs.length > 0 && (
+				<Grid container>
+					<Grid item md={1}></Grid>
+					<Grid item md={10}>
+						<TableContainer component={Paper}>
+							<Table className={classes.table} aria-label='simple table'>
+								<TableHead>
+									<TableRow>
+										<TableCell align='center'>Domain</TableCell>
+										<TableCell align='center'>URL</TableCell>
+										<TableCell align='center'>-</TableCell>
+									</TableRow>
+								</TableHead>
+								<TableBody>
+									{excludedURLs.map((url) => (
+										<TableRow key={url}>
+											<TableCell component='th' align='center' scope='row'>
+												{new URL(url).host}
+											</TableCell>
+											<TableCell align='center'>{url}</TableCell>
+											<TableCell align='center'>
+												<DeleteModal url={url} />
+											</TableCell>
+										</TableRow>
+									))}
+									<TableRow>
+										<TableCell scope='row' colSpan={3} padding='none'>
+											<DeleteAllModal />
+										</TableCell>
+									</TableRow>
+								</TableBody>
+							</Table>
+						</TableContainer>
+					</Grid>
+					<Grid item md={1}></Grid>
+				</Grid>
+			)}
+		</>
 	);
 }
+
+export default ExcludeURLList;
