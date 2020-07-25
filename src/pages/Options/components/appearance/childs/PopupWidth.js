730bddec9e02fdf6b086fa8d01cf181ab29a86bb
Abdul Samad <Samk8778@yahoo.com>
2020-07-26 00:14:33 +0530

    :speech_balloon: Centered default value text

--- src/pages/Options/components/appearance/childs/PopupWidth.js
+++ src/pages/Options/components/appearance/childs/PopupWidth.js
@@ -1,9 +1,22 @@
 import React, { useEffect, useState } from 'react';
-import { makeStyles, Grid, Typography, Slider } from '@material-ui/core';
+import {
+	makeStyles,
+	Grid,
+	Typography,
+	Slider,
+	FormHelperText,
+	FormControl,
+} from '@material-ui/core';
 
 const useStyles = makeStyles((theme) => ({
 	root: {
 		margin: '1.5rem 0',
+	},
+	formControl: {
+		width: '100%',
+	},
+	formHelperText: {
+		textAlign: 'center',
 	},
 }));
 
@@ -31,15 +44,18 @@ function PopupWidth() {
 				</Typography>
 			</Grid>
 			<Grid item md={4}>
-				<Slider
-					aria-labelledby='popup-width-slider'
-					min={250}
-					max={700}
-					step={10}
-					value={value}
-					onChange={onChange}
-					valueLabelDisplay='auto'
-				/>
+				<FormControl className={classes.formControl} hiddenLabel={true}>
+					<Slider
+						aria-labelledby='popup-width-slider'
+						min={250}
+						max={700}
+						step={10}
+						value={value}
+						onChange={onChange}
+						valueLabelDisplay='auto'
+					/>
+					<FormHelperText className={classes.formHelperText}>Default value is 400.</FormHelperText>
+				</FormControl>
 			</Grid>
 			<Grid item md={2}></Grid>
 		</Grid>
