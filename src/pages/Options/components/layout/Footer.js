877ac507ca2300b29fcc11b3784893534c0e38d2
Abdul Samad <Samk8778@yahoo.com>
2020-07-27 02:16:34 +0530

    :speech_balloon: Updated footer link

--- src/pages/Options/components/layout/Footer.js
+++ src/pages/Options/components/layout/Footer.js
@@ -15,7 +15,9 @@ function Footer({ title }) {
 		<footer className={classes.center}>
 			<Typography variant='subtitle1'>
 				Please leave a positive rating on the{' '}
-				<Link underline='none' href='https://chrome.google.com/webstore'>
+				<Link
+					underline='none'
+					href='https://chrome.google.com/webstore/detail/peek-history/gknodemjjckmkncijnedcpogffimkmbm?from=options'>
 					Chrome Web Store,
 				</Link>{' '}
 				If you like this extension and leave a feedback.
