86872e469767f82cd0dff369d017478a16d1a1c0
AbdulSamad Ansari <hello@abdulsamad.dev>
2022-07-09 20:18:38 +0530

    :construction: Added tablist component

---
+++ src/pages/popup/layout/tabs/TabsList.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+const TabsList = () => {
+  return (
+    <div>
+      <h1>
+        Hello World! Lorem ipsum dolor sit amet consectetur adipisicing elit.
+        Ut, fugiat. Reprehenderit repellendus placeat natus! Aspernatur ullam
+        voluptates corrupti sit at explicabo! Eveniet aut alias, a labore animi
+        dignissimos at, magnam iste quam ex temporibus consequuntur cum vitae
+        possimus debitis eaque neque, dolor enim incidunt repudiandae natus
+        laboriosam exercitationem tempore! Corporis odio fugit non nostrum
+        repudiandae modi laudantium dolorum similique ut quas, tempora
+        temporibus deserunt ratione nulla voluptate magnam vitae necessitatibus
+      </h1>
+    </div>
+  );
+};
+
+export default TabsList;
