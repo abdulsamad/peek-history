6e977aa9525a3e6a9c8c5124a5c43013041d3115
AbdulSamad Ansari <hello@abdulsamad.dev>
2022-07-13 13:02:25 +0530

    :wheelchair: Added html entity in footer

--- src/pages/options/layout/Footer.tsx
+++ src/pages/options/layout/Footer.tsx
@@ -7,9 +7,9 @@ const Footer = () => {
       <Typography variant="subtitle1">
         Please leave a positive rating on the{" "}
         <Link underline="none" href={import.meta.env.VITE_EXTENSION_URL}>
-          Chrome Web Store,
-        </Link>{" "}
-        If you like this extension and leave a feedback.
+          Chrome Web Store
+        </Link>
+        &#44; If you like this extension and leave a feedback.
       </Typography>
     </Container>
   );
