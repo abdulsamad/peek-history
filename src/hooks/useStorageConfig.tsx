0063bd84be7912129b9a266ade57b35fbdef3df5
AbdulSamad Ansari <hello@abdulsamad.dev>
2022-07-26 02:03:30 +0530

    :wastebasket: Removed change event

--- src/hooks/useStorageConfig.tsx
+++ src/hooks/useStorageConfig.tsx
@@ -1,33 +1,17 @@
-import { useState, useEffect, useLayoutEffect } from "react";
+import { useState, useEffect } from "react";
 
-import { settings } from "../pages/options/redux/settings/defaults";
+import { settings } from "../common/redux/settings/defaults";
 
 const useStorageConfig = () => {
   const [config, setConfig] = useState(settings);
 
   // Load initial data
-  useLayoutEffect(() => {
+  useEffect(() => {
     (async () => {
       // eslint-disable-next-line @typescript-eslint/no-explicit-any
       const syncVariables = (await chrome.storage.sync.get(null)) as any;
       setConfig(syncVariables);
     })();
-  }, []);
-
-  // Listen to changes
-  useEffect(() => {
-    // eslint-disable-next-line @typescript-eslint/no-explicit-any
-    const onChange = (data: any) => {
-      for (const key in data) {
-        setConfig({
-          ...config,
-          [key]: data[key].newValue,
-        });
-      }
-    };
-
-    chrome.storage.onChanged.addListener(onChange);
-    return () => chrome.storage.onChanged.removeListener(onChange);
   }, []);
 
   return config;
