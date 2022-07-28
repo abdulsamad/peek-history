144e12fc983e178fd5d8434e52558f9023ea3115
AbdulSamad Ansari <hello@abdulsamad.dev>
2022-07-28 16:08:25 +0530

    :lipstick: Improved search input UI

--- src/pages/popup/layout/Search.tsx
+++ src/pages/popup/layout/Search.tsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useRef } from "react";
 import { useSelector } from "react-redux";
-import { InputBase, styled } from "@mui/material";
+import { lighten, InputBase, styled } from "@mui/material";
 import { Search as SearchIcon } from "@mui/icons-material";
 
 import { RootState, useAppDispatch } from "../redux/store";
@@ -18,26 +18,40 @@ const StyledSearchIcon = styled(SearchIcon)(({ theme }) => ({
   color: theme.palette.primary.light,
 }));
 
-const StyledInputBase = styled(InputBase)<{ open: boolean }>(
-  ({ theme, open }) => ({
-    transition: "all 0.3s ease-out",
-    trasformOrigin: "left",
-    background: "transparent",
-    cursor: "pointer",
-    position: "absolute",
-    top: 0,
-    right: 0,
-    bottom: 0,
-    height: 60,
-    width: open ? "100%" : 0,
-    padding: "8px 1rem",
-    zIndex: open ? 10 : -1,
-    backgroundColor: theme.palette.background.default,
-  })
-);
+const StyledInputBase = styled(InputBase)<{
+  open: boolean;
+  accentFontColor: string;
+}>(({ theme, open, accentFontColor }) => ({
+  transition: "all 0.3s ease-out",
+  trasformOrigin: "left",
+  background: theme.palette.primary.main,
+  cursor: "pointer",
+  position: "absolute",
+  top: 0,
+  right: 0,
+  bottom: 0,
+  height: 60,
+  width: open ? "100%" : 0,
+  padding: "8px 1rem",
+  zIndex: open ? 10 : -1,
+
+  input: {
+    padding: "0.5rem",
+    borderRadius: 10,
+    backgroundColor: "rgba(255,255,255, 0.35)",
+    color: "#f5f5f5",
+
+    "&::placeholder": {
+      color: lighten(accentFontColor, 0.9),
+    },
+  },
+}));
 
 const Search = () => {
   const UI = useSelector((state: RootState) => state.ui);
+  const accentFontColor = useSelector(
+    (state: RootState) => state.settings.accentFont
+  );
   const inputRef = useRef<HTMLInputElement>(null);
 
   const dispatch = useAppDispatch();
@@ -54,6 +68,8 @@ const Search = () => {
         onClick={() => dispatch(setSearchOpened(!UI.searchOpened))}
       />
       <StyledInputBase
+        open={UI.searchOpened}
+        accentFontColor={accentFontColor}
         placeholder="Enter search keyword or URL"
         inputProps={{ "aria-label": "search history" }}
         inputRef={inputRef}
@@ -65,18 +81,17 @@ const Search = () => {
             // Update history
             await dispatch(getHistory({ text: target.value }));
             return;
+          } else if (UI.active === Active.TABS) {
+            // ! BUG: Fix state not properly updating because of Immer in tabSlice. Other Tabs filtering also needs work.
+            // Refresh data (because filtering removed the data from tabSlice state)
+            dispatch(getRecentlyClosed());
+            dispatch(getDevices());
+
+            // Update Tabs
+            dispatch(filter(target.value));
           }
-
-          // ! BUG: Fix state not properly updating because of Immer in tabSlice. Other Tabs filtering also needs work.
-          // Refresh data (because filtering removed the data from tabSlice state)
-          dispatch(getRecentlyClosed());
-          dispatch(getDevices());
-
-          // Update Tabs
-          dispatch(filter(target.value));
         }}
         onBlur={() => dispatch(setSearchOpened(false))}
-        open={UI.searchOpened}
       />
     </div>
   );
