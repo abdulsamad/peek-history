fa4186b7b869e42d6ef573106b6ad08af56f650b
AbdulSamad Ansari <hello@abdulsamad.dev>
2023-02-27 15:02:37 +0530

    :bulb: Updated tabs thunk comment

--- src/pages/popup/redux/tabs/thunks.ts
+++ src/pages/popup/redux/tabs/thunks.ts
@@ -6,14 +6,14 @@ import { openAllTabsAccordions } from "../../hooks/utils";
 export const getRecentlyClosed = createAsyncThunk(
   "tabs/getRecentlyClosed",
   async () => {
-    // ! @types/chrome v0.0.193 is not updated as per manifest v3. This should return promise
+    // ! @types/chrome is not updated as per manifest v3. This should return promise
     const tabs = await (chrome.sessions as any).getRecentlyClosed();
     return tabs;
   }
 );
 
 export const getDevices = createAsyncThunk("tabs/getDevices", async () => {
-  // ! @types/chrome v0.0.193 is not updated as per manifest v3. This should return promise
+  // ! @types/chrome is not updated as per manifest v3. This should return promise
   const deviceItems = await (chrome.sessions as any).getDevices();
   return deviceItems;
 });
